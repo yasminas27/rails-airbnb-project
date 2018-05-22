4807d9dc0e5d7d2808ca156e0bb517cb624bc1f2
Pierre Cohen <pierre-cohen@hotmail.fr>
2018-05-22 18:54:57 +0200

    home page + search form ok

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,18 +1,28 @@
 import Typed from 'typed.js';
 
 function reloadBannerImage() {
-  window.setInterval(changeImage, 2000);
+  window.setInterval(changeImage, 10000);
 }
 
 function changeImage() {
   const banner = document.querySelector(".banner");
   const images = banner.dataset.images.split(',');
   // find which image is displayed
-  console.log(banner.style.backgroundImage);
+  const backgroundImage = window.getComputedStyle(banner).backgroundImage;
+  const backgroundImageUrl = /url\("(.*)"\)/.exec(backgroundImage)[1];
+  // console.log(Boolean(images[0] === backgroundImageUrl))
+  let index = images.indexOf(backgroundImageUrl);
+  let newBackgroundImageUrl = '';
+  if(index < 2) {
+    index += 1
+    newBackgroundImageUrl = images[index];
+  } else {
+    newBackgroundImageUrl = images[0];
+  }
   // find index in the images array
   // take next image if index < 2
   // else take first image
-  // banner.style.backgroundImage = `url(${images[1]})`;
+  banner.style.backgroundImage = `linear-gradient(-225deg, rgba(0, 101, 168, 0.6) 0%, rgba(0, 36, 61, 0.6) 50%), url(${newBackgroundImageUrl})`;
 }
 
 function loadDynamicBannerText() {
