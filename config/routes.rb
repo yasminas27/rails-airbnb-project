53127b2ca01a713d72d996f508c181267d5d5605
Adeline Blanc <blanc.foix@gmail.com>
2018-05-22 14:57:39 +0200

    show families

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :families, only: [:show]
+  resources :families, only: [:show, :index]
   get "/families?capacity=[:capacity]&price=[:price_pppn]", to: "families#index"
 end
