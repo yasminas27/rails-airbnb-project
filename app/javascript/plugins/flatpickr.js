fdf05a6e4bc3f2c958e3df79fa83f9d46340634e
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-24 14:26:25 +0200

    editing the index view of bookings

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,9 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
 
-flatpickr(".datepicker", {})
+
+flatpickr("#range_start", {
+  allowInout: true,
+  altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
