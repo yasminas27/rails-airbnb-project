c568bf561d94aabaea8002e8f660dd2bd726aa62
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-25 10:53:25 +0200

    linked to Booking in Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :families do
-    resources :bookings
+    resources :bookings, except: [:index, :show]
   end
+
+  get "profile", to: "pages#profile", as: :profile
+
 end
