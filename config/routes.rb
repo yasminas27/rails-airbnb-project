781de7271d81d093f4ba3dbe9fb5e8b8428d96ee
Yasmina Saradar <yasminasaradar@gmail.com>
2018-05-25 11:11:00 +0200

    modification routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :families do
-    resources :bookings, except: [:index, :show]
+    resources :bookings, except: [:index]
   end
 
   get "profile", to: "pages#profile", as: :profile
