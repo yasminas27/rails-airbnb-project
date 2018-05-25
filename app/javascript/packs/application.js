997fabf5f11fddba4caa115d505db24b7464232a
Pierre Cohen <pierre-cohen@hotmail.fr>
2018-05-25 13:35:07 +0200

    nothing more to say

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,6 @@
 import "bootstrap";
 import "../plugins/flatpickr";
 import { loadDynamicBannerText, reloadBannerImage } from '../components/banner';
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-initUpdateNavbarOnScroll();
 
 loadDynamicBannerText();
 
